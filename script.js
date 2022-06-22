598bd86c765106d39d0fd83561de567b5007470b
samuelconlon <conlonsp@yahoo.com>
2022-06-22 17:41:25 -0400

    created p1, added text and appended to index.html via script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const header1 = document.querySelector("h1");
 header1.style.textAlign = "center";
 header1.style.fontSize = "65px";
+
+const p1 = document.createElement("p");
+p1.textContent = "The purpose of this Mock Site is to provide coding practice for our CREATOR, the benevolent Sam Conlon.  Praise Him!  For He is our salvation and the sweetest nectar of the Universe.  Shall He live forever.  Amen.";
+document.querySelector("body").appendChild(p1);
